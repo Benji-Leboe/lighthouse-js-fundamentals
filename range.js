0b106f2706450b88e0a32533271dfe7fa362752b
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-07-10 02:43:26 +0000

    new range function written to try and pass evaluation; webpage keeps crashing

--- range.js
+++ range.js
@@ -1,18 +1,12 @@
 function range(start, end, step){
-  var current = start;
   var listed = [];
-  while(current <= end){
-    listed.push(current);
-    current += step;
+  for(var i = start; i <= end; i += step){
+    listed.push(i);
   }
-  if(listed.length > 0){
-    return listed; 
-  }else{
-    return undefined;
-  }  
+  return listed;
 }
 
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
-console.log(range());
+
