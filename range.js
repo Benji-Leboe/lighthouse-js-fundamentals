9785fd40b1f2faf83182979222b2f75c2f4a654a
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-08-20 19:35:05 +0000

    fixed range function for LHL

--- range.js
+++ range.js
@@ -1,7 +1,9 @@
 function range(start, end, step){
   var listed = [];
-  for(var i = start; i <= end; i += step){
-    listed.push(i);
+  if(start < end && step > 0 && (start || start === 0) && end && step){
+    for(var i = start; i <= end; i += step){
+      listed.push(i);
+    }
   }
   return listed;
 }
@@ -9,4 +11,6 @@ function range(start, end, step){
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+console.log(range());
+console.log(range(20, 10, 1));
 
